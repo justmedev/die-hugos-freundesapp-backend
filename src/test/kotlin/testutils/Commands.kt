5bc74cd16a2e41bec80b7fee9a5ccedc0e006d1f
Ilja Busch <ilja.ragnar@gmail.com>
2026-05-16 19:36:12 +0200

    fix: tests failed because of duplicae emails

--- src/test/kotlin/testutils/Commands.kt
+++ src/test/kotlin/testutils/Commands.kt
@@ -3,12 +3,18 @@ package testutils
 import domain.commands.CreateUserCommand
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.todayIn
+import kotlin.concurrent.atomics.AtomicInt
+import kotlin.concurrent.atomics.ExperimentalAtomicApi
+import kotlin.concurrent.atomics.incrementAndFetch
 import kotlin.time.Clock
 
+@OptIn(ExperimentalAtomicApi::class)
 object Commands {
     object User {
+        private val nextId: AtomicInt = AtomicInt(0)
+
         fun create(
-            email: String = "a@b.c",
+            email: String = "user${nextId.incrementAndFetch()}@b.c",
             firstName: String = "Max",
             lastName: String = "Mustermann",
             isAdmin: Boolean = false
