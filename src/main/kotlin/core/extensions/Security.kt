d5ee0ef01ae2bda25eae85113c97840ee2f35aba
Ilja Busch <ilja.ragnar@gmail.com>
2026-06-09 16:39:34 +0200

    fix: requireUserId returned KeycloakId

--- src/main/kotlin/core/extensions/Security.kt
+++ src/main/kotlin/core/extensions/Security.kt
@@ -4,17 +4,18 @@ import core.exceptions.Unauthorized
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.auth.jwt.*
+import io.ktor.server.plugins.di.*
+import service.user.UserService
 
-fun ApplicationCall.requireUserId(): Int = principal<JWTPrincipal>()?.payload?.subject?.toIntOrNull()
-    ?: throw Unauthorized()
+suspend fun ApplicationCall.requireUserId(): Int {
+    val kid = requireKeycloakId()
+    return application.dependencies.resolve<UserService>().findByKeycloakId(kid).id
+}
 
 fun ApplicationCall.requireKeycloakId(): String = principal<JWTPrincipal>()?.payload?.subject
     ?: throw Unauthorized()
 
-fun ApplicationCall.requireUserRole(): String = principal<JWTPrincipal>()?.payload?.claims?.get("role")?.asString()
-    ?: throw Unauthorized()
-
-suspend fun ApplicationCall.requireUser(userService: service.user.UserService): domain.models.User {
+suspend fun ApplicationCall.requireUser(userService: UserService): domain.models.User {
     val keycloakId = requireKeycloakId()
     return userService.findByKeycloakId(keycloakId)
 }
