63ff91c6785411b743cf98db2cbc9bfcdacdf1d1
Ilja Busch <ilja.ragnar@gmail.com>
2026-07-20 23:39:45 +0200

    feat: call logging includes route, clearer formatting

--- src/main/kotlin/plugins/CallLogging.kt
+++ src/main/kotlin/plugins/CallLogging.kt
@@ -3,14 +3,16 @@ package plugins
 import io.ktor.server.application.*
 import io.ktor.server.plugins.calllogging.*
 import io.ktor.server.request.httpMethod
+import io.ktor.server.request.location
 
 fun Application.configureCallLogging() {
     install(CallLogging) {
         format { call ->
             val status = call.response.status()
             val httpMethod = call.request.httpMethod.value
+            val route = call.request.local.uri
             val userAgent = call.request.headers["User-Agent"]
-            "Status: $status, HTTP method: $httpMethod, User agent: $userAgent"
+            "$httpMethod $route -> $status, User agent: $userAgent"
         }
     }
 }
